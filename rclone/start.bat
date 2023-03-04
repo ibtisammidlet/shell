22ccfb47a8e4b1af75eff370990bbd2d513538a8
ibtisam midlet <storebitid@gmail.com>
2023-03-04 11:50:23 -0800

    *

--- rclone/start.bat
+++ rclone/start.bat
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-rclone mount ibtisam: X: --vfs-cache-max-size 2048M  --retries 10
+rclone mount ibtisam2: X: --vfs-cache-max-size 2048M  --retries 10
 :: rclone  --vfs-cache-max-size 2048M  --retries 10 mount mega: M:
 
 :: --password-command echo newstlone
 :: https://forum.rclone.org/t/rclone-inside-a-script-safely-disable-password-prompts-with-no-fail/14459/3
+:: rclone rcd --rc-web-gui --rc-user user --rc-pass password
