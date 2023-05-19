83f16ee33c70974691ae832ce0a32308c5b6960d
ibtisam midlet <storebitid@gmail.com>
2023-05-19 19:32:49 +0100

    *

---
+++ dts.bat
@@ -0,0 +1,5 @@
+:: wsreset.exe
+:: winget install 9PDJ0C6SDT6F --accept-package-agreements
+:: https://answers.microsoft.com/en-us/windows/forum/all/my-dts-studio-sound-is-gone/f5d4faca-ac11-47d0-9d2e-597e86163254
+
+start "" "E:\data 2\browsers\GoogleChromePortable\GoogleChromePortable.exe" "https://h30434.www3.hp.com/t5/Notebook-Audio/DTS-sound-control-panel/td-p/6130651"
