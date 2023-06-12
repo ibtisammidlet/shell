a69dece4fbb07ae8db2137780a5fec6e3d9bf8dc
ibtisam midlet <storebitid@gmail.com>
2023-06-12 18:12:09 +0100

    *

--- idle/brenchs.bat
+++ idle/brenchs.bat
@@ -18,9 +18,13 @@ for /f "tokens=*" %%B in ('git branch -r') do (
   git branch --track "%%B" "%%B"
   git checkout "%%B"
   git pull
-  mkdir "%%B"
-  xcopy /E /I /Y * ".\%%B\"
-  git clean -xdf
+  mkdir "%%~nxB"
+  robocopy . "%%~nxB" /E /XD .git
+)
+
+echo Moving branches to respective directories...
+for /d %%D in (temp\*) do (
+  move "%%D" "%%~nxD"
 )
 
 echo Cleaning up...
@@ -30,3 +34,5 @@ rd /S /Q temp
 
 echo All branches downloaded successfully.
 pause
+
+REM time consumbtion was too high
