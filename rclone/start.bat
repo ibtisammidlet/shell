ed881bc63aa46c1bf5c48d1b169bf953a88c62aa
ibtisam midlet <storebitid@gmail.com>
2024-03-05 18:01:32 +0100

    0

--- rclone/start.bat
+++ rclone/start.bat
@@ -32,13 +32,13 @@ set password=start
 
 start cmd /c "rclone mount ibtisam2: X: --vfs-cache-max-size 20480M --retries 10 --vfs-cache-mode full --vfs-cache-max-age 8640h -vv --cache-dir E:\data\development\shell\rclone\cache --allow-other --allow-non-empty --buffer-size 32M --dir-cache-time 8640h --attr-timeout 8640h --drive-chunk-size 32M --vfs-read-chunk-size 32M --vfs-read-ahead 32M --transfers 16 --checkers 16  --password-command  "cmd /c echo %password%"" 
 
-::timeout /t 5
-
-::start cmd /c "rclone mount elena: N: --vfs-cache-max-size 20480M --retries 10 --vfs-cache-mode full --vfs-cache-max-age 8640h -vv --cache-dir E:\data\development\shell\rclone\cache --allow-other --allow-non-empty --buffer-size 32M --dir-cache-time 8640h --attr-timeout 8640h --drive-chunk-size 32M --vfs-read-chunk-size 32M --vfs-read-ahead 32M --transfers 16 --checkers 16  --password-command  "cmd /c echo %password%"" 
-
 timeout /t 5
 
-start cmd /c "rclone mount mega: M: --vfs-cache-max-size 20480M --retries 10 --vfs-cache-mode full --vfs-cache-max-age 8640h -vv --cache-dir E:\data\development\shell\rclone\cache --allow-other --allow-non-empty --buffer-size 32M --dir-cache-time 8640h --attr-timeout 8640h --drive-chunk-size 32M --vfs-read-chunk-size 32M --vfs-read-ahead 32M --transfers 16 --checkers 16  --password-command  "cmd /c echo %password%"" 
+start cmd /c "rclone mount elena: N: --vfs-cache-max-size 20480M --retries 10 --vfs-cache-mode full --vfs-cache-max-age 8640h -vv --cache-dir E:\data\development\shell\rclone\cache --allow-other --allow-non-empty --buffer-size 32M --dir-cache-time 8640h --attr-timeout 8640h --drive-chunk-size 32M --vfs-read-chunk-size 32M --vfs-read-ahead 32M --transfers 16 --checkers 16  --password-command  "cmd /c echo %password%"" 
+
+::timeout /t 5
+
+::start cmd /c "rclone mount mega: M: --vfs-cache-max-size 20480M --retries 10 --vfs-cache-mode full --vfs-cache-max-age 8640h -vv --cache-dir E:\data\development\shell\rclone\cache --allow-other --allow-non-empty --buffer-size 32M --dir-cache-time 8640h --attr-timeout 8640h --drive-chunk-size 32M --vfs-read-chunk-size 32M --vfs-read-ahead 32M --transfers 16 --checkers 16  --password-command  "cmd /c echo %password%"" 
 
 pause 
  
